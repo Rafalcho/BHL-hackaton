d9e988befa5260c83ae2bbe373482eabbf22f5c4
Rafal Pytel <rafal.pytel1@wp.pl>
2017-05-28 00:56:39 +0200

    adding parties done

--- webpage/js/script.js
+++ webpage/js/script.js
@@ -1,17 +1,19 @@
 var myPosition;
 var choosePoint = false;
+var map;
+var parties=[];
 function initMap() {
 
   var markersToShow = [];
   myPosition = new google.maps.LatLng(52.229802,21.011818);
-  var map = new google.maps.Map(document.getElementById('map'), {
+   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: {lat: 52.229802, lng: 21.011818},
     disableDefaultUI: true
   });
 
   const fetchMarkers = () => {
-    let url = 'http://10.78.25.34:8080/patrols/?x='+myPosition.lat()+'&y='+myPosition.lng()+'&rad=100';
+    let url = 'http://10.78.16.243:8080/patrols/?x='+myPosition.lat()+'&y='+myPosition.lng()+'&rad=100';
     fetch(url).then(response => {
       if (response.ok) {
         return response.json();
@@ -96,15 +98,18 @@ function initMap() {
       title: 'Hello World!'
     });
     myCity.setMap(map);
-
+    google.maps.event.addListener(map, 'click', function(e) {
+      placeMarker(e.latLng, map);
+    });
   }
   google.maps.event.addListener(map, 'click', function(e) {
     placeMarker(e.latLng, map);
   });
 
   function placeMarker(position, map) {
+    console.log(choosePoint);
     if(choosePoint){
-      sendPost(position);
+      sendPostBaggeteu(position);
       choosePoint=false;
     }
   }
@@ -115,13 +120,13 @@ function addBaguette(){
 }
 function sendAlert(){
   document.getElementById("AddBaguetteAlert").style.visibility="hidden";
-  sendPost(myPosition);
+  sendPostBaggeteu(myPosition);
 }
 function chooseSpot(){
   document.getElementById("AddBaguetteAlert").style.visibility="hidden";
   choosePoint = true;
 }
-function sendPost(position){
+function sendPostBaggeteu(position){
   var payload = {
     x: position.lat(),
     y: position.lng(),
@@ -129,8 +134,8 @@ function sendPost(position){
   };
 
   var data = JSON.stringify( payload );
-  
-  fetch("http://10.78.25.34:8080/patrols/",
+
+  fetch("http://10.78.16.243:8080/patrols/",
     {
       method: "POST",
       body: data
@@ -138,5 +143,90 @@ function sendPost(position){
     .then(function(res){ return res.json(); })
     .then(function(data){  })
 }
+
+function sendPostParty(position){
+  var payload = {
+    x: position.lat(),
+    y: position.lng(),
+    name: document.getElementById("partyName").value,
+    description: document.getElementById("partyDescription").value
+  };
+
+  var data = JSON.stringify( payload );
+console.log(data);
+  fetch("http://10.78.16.243:8080/parties/",
+    {
+      method: "POST",
+      body: data
+    })
+    .then(function(res){ return res.json(); })
+    .then(function(data){  })
+}
+function makeParty(){
+  document.getElementById("partyForm").style.visibility="hidden";
+  sendPostParty(myPosition);
+
+
+}
+function abortParty(){
+  document.getElementById("partyForm").style.visibility="hidden";
+
+}
+function addParty(){
+  document.getElementById("partyForm").style.visibility="visible";
+}
+function showParty(){
+  getParties();
+}
+const getParties = () => {
+  let url = 'http://10.78.16.243:8080/parties/?x='+myPosition.lat()+'&y='+myPosition.lng()+'&rad=100';
+  fetch(url).then(response => {
+    if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Fetching markers failed');
+  }
+})
+.then(response => {
+    const data = JSON.parse(response);
+  console.log(response);
+
+  data.forEach(marker => {
+    let newMarker = new google.maps.Marker({
+      position: {lat: marker.x, lng: marker.y},
+      animation: google.maps.Animation.BOUNCE,
+      icon: 'https://cdn1.iconfinder.com/data/icons/party-3/500/Party_2-128.png',
+      map: map,
+      title: 'Hello World!'
+    });
+var content=marker.name+'<br/>Opis:'+marker.description+
+  '<br/><button id="'+marker.description+'" onclick="joinParty()">Dołącz</button><br/>';
+  let infowindow = new google.maps.InfoWindow({
+    content: content
+  });
+
+  // infowindow.open(map, newMarker);
+  newMarker.addListener('click', function() {
+      infowindow.open(map,newMarker);
+    }
+  );
+  parties.push(newMarker);
+});
+
+})
+.catch(error => {
+    console.log('Fetching data error:', error);
+});
+};
+
+function joinParty(){
+  console.log(event.currentTarget);
+  var name = event.currentTarget.id;
+  console.log(name);
+  
+
+
+}
+
 
 
