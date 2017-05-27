954881a91e5f1bb0eb9592f7befeb6f05d0bb769
Rafal Pytel <rafal.pytel1@wp.pl>
2017-05-27 18:25:10 +0200

    working with your position

--- webpage/js/script.js
+++ webpage/js/script.js
@@ -63,4 +63,43 @@ function initMap() {
     console.log(markersToShow);
   }, 30000);
 
+
+  ///------------your position renerding--------------
+
+  var myPosition = new google.maps.LatLng(52.229802,21.011818);
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      x.innerHTML = 'Geolocation is not supported by this browser.';
+    }
+  }
+
+  function showPosition(position) {
+    myPosition = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    drawMap();
+    fetchMarkers();
+
+  }
+  // drawMap();
+   getLocation();
+
+  function drawMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: myPosition
+    });
+
+    var myCity = new google.maps.Circle({
+      center: myPosition,
+      radius: 150,
+      strokeColor: '#0000FF',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#0000FF',
+      fillOpacity: 0.4
+    });
+    myCity.setMap(map);
+  }
 }
