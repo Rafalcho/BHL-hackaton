fcf9b0cb9647722f4b254957e8221f367947319c
Kamil Magryta <gryxon@gmail.com>
2017-05-27 16:01:56 +0200

    Adding materialize design.

--- webpage/js/script.js
+++ webpage/js/script.js
@@ -1,9 +1,9 @@
 function initMap() {
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: {lat: 52.229802, lng: 21.011818}
-  });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lat: 52.229802, lng: 21.011818}
+    });
 
 }
 
