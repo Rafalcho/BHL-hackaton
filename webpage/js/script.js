1e53561fe0a2eac83af1d21be578785dfc6de441
Rafalcho <raf.cholaj@gmail.com>
2017-05-27 16:08:13 +0200

    markers fetch

--- webpage/js/script.js
+++ webpage/js/script.js
@@ -1,10 +1,34 @@
 function initMap() {
 
+  var markersToShow = [];
+
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: {lat: 52.229802, lng: 21.011818}
   });
 
-}
+  var marker = new google.maps.Marker({
+    position: {lat: 52.239802, lng: 21.011818},
+    map: map,
+    title: 'Hello World!'
+  });
 
-initMap();
+  const fetchMarkers = () => {
+    fetch(url).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Fetching markers failed');
+      }
+    })
+    .then(response => {
+
+      return response.items;
+
+    })
+    .catch(error => {
+      console.log('Fetching data error:', error);
+    });
+  };
+
+}
